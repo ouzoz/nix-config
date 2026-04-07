83f65cfa2dbc1cbc7e18b3d6599218bc3c41a84d
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-07 12:12:17 +0300

    bash

--- bash/bash.nix
+++ bash/bash.nix
@@ -20,42 +20,38 @@
     promptInit = "";
     shellAliases = {
       # shortcuts
-      l = "ls -ACxX --group-directories-first --color=auto"
-      
+      l = "ls -ACxX --group-directories-first --color=auto";
       # loc
-      tok = "tokei -C -s lines -n dots"
-      tokf = "tok -f"
-      lan = "github-linguist"
-      
+      tok = "tokei -C -s lines -n dots";
+      tokf = "tok -f";
+      lan = "github-linguist";
       # nix
-      conf-dir = "cd /etc/nixos"
-      conf-edit = "vi /etc/nixos/configuration.nix"
-      conf-build = "sudo nixos-rebuild switch --flake /etc/nixos#ouz"
-      conf-update = "sudo nix flake update"
-      conf-gc = "sudo nix-collect-garbage -d"
-      conf-direnv = "echo 'use flake' > .envrc && direnv allow"
-      conf-flake = "nix develop"
-      conf-template = "nix flake init -t git+ssh://git@github.com/oguzhanozkaya/nix-flake-templates && direnv allow"
-      conf-search = "nix search nixpkgs"
-      
+      conf-dir = "cd /etc/nixos";
+      conf-edit = "vi /etc/nixos/configuration.nix";
+      conf-build = "sudo nixos-rebuild switch --flake /etc/nixos#ouz";
+      conf-update = "sudo nix flake update";
+      conf-gc = "sudo nix-collect-garbage -d";
+      conf-direnv = "echo 'use flake' > .envrc && direnv allow";
+      conf-flake = "nix develop";
+      conf-template = "nix flake init -t git+ssh://git@github.com/oguzhanozkaya/nix-flake-templates && direnv allow";
+      conf-search = "nix search nixpkgs";
       # git
-      gs = "git status"
-      gc = "git commit -m"
-      ga = "git add"
-      gp = "git push"
-      gpu = "git pull"
-      gd = "git diff"
-      gl = "git log --oneline -12"
-      gi = "gd --stat"
+      gs = "git status";
+      gc = "git commit -m";
+      ga = "git add";
+      gp = "git push";
+      gpu = "git pull";
+      gd = "git diff";
+      gl = "git log --oneline -12";
+      gi = "gd --stat";
       # alias gr = "git reset HEAD"
       # alias m = "git checkout master"
       # alias gch = "git checkout"
       # alias gm = "git merge"
-      
       # commands
-      oz-token = "openssl rand -hex 16"
-      oz-ram = "top -b -o +RES -n 1 -Em -em | head -n 60"
-      oz-app-start = "systemctl list-unit-files --type=service | grep enabled"
+      oz-token = "openssl rand -hex 16";
+      oz-ram = "top -b -o +RES -n 1 -Em -em | head -n 60";
+      oz-app-start = "systemctl list-unit-files --type=service | grep enabled";
     };
   };
 }
