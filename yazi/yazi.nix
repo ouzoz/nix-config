4f9dacc0e37b32531934c3c8b61f50b792690698
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-07 12:47:36 +0300

    yazi

--- yazi/yazi.nix
+++ yazi/yazi.nix
@@ -4,13 +4,11 @@
     enable = true;
     settings = {
       yazi = {
-        mgr = {
-          ratio = "[ 1, 3, 2 ]";
-          sort_by = "extension";
-          linemode = "mtime"; # size
-          show_hidden = true;
-          show_symlink = true;
-        };
+        ratio = [ 1 3 2 ];
+        sort_by = "extension";
+        linemode = "mtime"; # size
+        show_hidden = true;
+        show_symlink = true;
       };
       theme = lib.importTOML ./theme.toml;
     };
