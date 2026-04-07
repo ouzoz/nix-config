9b68fc023f20c11769cac1d2e716d56ffa1e7067
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-07 12:45:52 +0300

    yazi

--- yazi/yazi.nix
+++ yazi/yazi.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       yazi = {
-        manager = {
+        mgr = {
           ratio = "[ 1, 3, 2 ]";
           sort_by = "extension";
           linemode = "mtime"; # size
