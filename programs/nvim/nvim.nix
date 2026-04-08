7762deab749720537bde6c4fb595a4ab1d31992e
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-08 04:47:08 +0300

    nvim

--- programs/nvim/nvim.nix
+++ programs/nvim/nvim.nix
@@ -4,5 +4,13 @@
     enable = true;
     defaultEditor = true;
     viAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+    configure = {
+      customRC = ''
+        set runtimepath^=/etc/nixos/programs/nvim
+        luafile /etc/nixos/programs/nvim/init.lua
+      '';
+    };
   };
 }
