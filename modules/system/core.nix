ad5f5228fc8ddef1cf2f8b550d658d133d96ae3a
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-09 11:54:43 +0300

    empty template

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -14,14 +14,15 @@
     openconnect
   ];
 
-programs.direnv = {
-  enable = true;
-  nix-direnv.enable = true;
-  settings = {
-    global = {
-      hide_env_diff = true;
-      warn_timeout = "5m";
-      log_format = "";
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    settings = {
+      global = {
+        hide_env_diff = true;
+        warn_timeout = "5m";
+        log_format = "";
+      };
     };
   };
 }
