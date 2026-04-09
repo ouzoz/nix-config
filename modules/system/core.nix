3ab22cbcf0361eda94eef64ce973d8df9785cf65
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-09 11:53:53 +0300

    empty template

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -14,9 +14,14 @@
     openconnect
   ];
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    settings.global.log_format = "-";
+programs.direnv = {
+  enable = true;
+  nix-direnv.enable = true;
+  settings = {
+    global = {
+      hide_env_diff = true;
+      warn_timeout = "5m";
+      log_format = "";
+    };
   };
 }
--- templates/empty/flake.nix
+++ templates/empty/flake.nix
@@ -3,16 +3,29 @@
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
   outputs = { self, nixpkgs }:
   let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
+    devShells = forAllSystems (system: 
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
 
-      ];
-      shellHook = ''
-        echo "empty flake loaded."
-      '';
-    };
+          ];
+
+          shellHook = ''
+            echo "Project Packages and environment loaded for ${system}."
+          '';
+        };
+      }
+    );
   };
 }
