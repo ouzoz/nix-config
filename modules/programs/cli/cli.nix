076b34d432f1baeb53e4fa57dc156dc791dc1006
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-14 01:26:07 +0300

    rename

--- modules/programs/cli/cli.nix
+++ modules/programs/cli/cli.nix
@@ -25,7 +25,7 @@
       ln -sfn /etc/nixos/modules/programs/cli/opencode/commands ~/.config/opencode/commands
       ln -sfn /etc/nixos/modules/programs/cli/opencode/themes ~/.config/opencode/themes
       ln -sfn /etc/nixos/modules/programs/cli/opencode/opencode.json ~/.config/opencode/opencode.json
-      ln -sfn /etc/nixos/modules/programs/cli/opencode/AGENTS.md ~/.config/opencode/RULES.md
+      ln -sfn /etc/nixos/modules/programs/cli/opencode/RULES.md ~/.config/opencode/RULES.md
     '';
   };
 }
