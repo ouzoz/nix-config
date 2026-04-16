3777f76a6f736151e54db6b30e77fb3cd7cab581
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-17 01:37:30 +0300

    nix.nix style

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,14 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than 6d";
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 6d";
+    };
   };
+
+  nixpkgs.config.allowUnfree = true;
 
   environment.shellAliases = {
     # helpers
