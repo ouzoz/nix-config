7bb539bd449b98578c7e093f86864e1f87ad5856
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-30 02:18:52 +0300

    Prş 30 Nis 2026 02:18:52 +03

--- modules/programs/development.nix
+++ modules/programs/development.nix
@@ -1,24 +1,13 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    android-tools
-  ];
-
   programs.wireshark = {
     enable = true;
     package = pkgs.wireshark;
     dumpcap.enable = true;
   };
 
-  virtualisation.libvirtd.enable = true;
-
   users.users.ouz.extraGroups = [
-    #adb
-    "kvm"
-    "libvirtd"
-
-    #wireshark
     "wireshark"
   ];
 }
