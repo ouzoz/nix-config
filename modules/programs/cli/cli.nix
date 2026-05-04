3c6406078403c421309da2fcdc41bb3add9dbc40
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-04 22:34:53 +0300

    Pzt 04 May 2026 22:34:53 +03

--- modules/programs/cli/cli.nix
+++ modules/programs/cli/cli.nix
@@ -13,7 +13,6 @@
   };
   environment.systemPackages = with pkgs; [
     opencode
-    fastfetch
     tokei
     github-linguist
   ];
--- modules/system/core.nix
+++ modules/system/core.nix
@@ -18,6 +18,7 @@
   environment.systemPackages = with pkgs; [
     just
     ripgrep
+    fastfetch
     wget
     zip
     unzip
