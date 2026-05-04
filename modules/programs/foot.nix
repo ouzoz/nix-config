cb2f625ed2edbf49d55dba9cc67f7a622979c89b
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-04 14:27:17 +0300

    Pzt 04 May 2026 14:27:17 +03

--- modules/programs/foot.nix
+++ modules/programs/foot.nix
@@ -27,23 +27,23 @@
         foreground = "000000";
         background = "ffffff";
 
-        regular0 = "000000";
+        regular0 = "cbc6d0";
         regular1 = "ff3a4c";
         regular2 = "00b708";
         regular3 = "cccb00";
         regular4 = "6687ff";
         regular5 = "bd56ff";
         regular6 = "00ad9f";
-        regular7 = "cbc6d0";
+        regular7 = "000000";
 
-        bright0 = "65616a";
+        bright0 = "ffffff";
         bright1 = "ff3a4c";
         bright2 = "00b708";
         bright3 = "cccb00";
         bright4 = "6687ff";
         bright5 = "bd56ff";
         bright6 = "00ad9f";
-        bright7 = "ffffff";
+        bright7 = "65616a";
 
         "233" = "100d14";
         "235" = "232027";
