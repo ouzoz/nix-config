8630b5cb16bde4de3f09eefc2ec69b5b3f030dda
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-06 16:27:56 +0300

    Çrş 06 May 2026 16:27:56 +03

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -3,13 +3,10 @@
 {
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  # boot.kernelModules = [
-  #   "nvidia"
-  #   "nvidia_modeset"
-  #   "nvidia_drm"
-  #   "nvidia_uvm"
-  # ];
+
+  # boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_drm" "nvidia_uvm" ];
   # hardware.nvidia.forceFullCompositionPipeline = true;
+
   hardware.nvidia = {
     nvidiaPersistenced = true;
     modesetting.enable = true;
@@ -21,34 +18,39 @@
     prime = {
       intelBusId = "PCI:0:2:0"; 
       nvidiaBusId = "PCI:1:0:0";
-
-      # offload.enable = false;
-      # sync.enable = false;
-      # reverseSync.enable = false;
     };
   };
 
-  environment.sessionVariables = {
-    KWIN_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
-    # GBM_BACKEND = "nvidia-drm";
-    # LIBVA_DRIVER_NAME = "nvidia";
-  };
+  # environment.sessionVariables = {
+  #   KWIN_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
+  # };
+
   # environment.sessionVariables = {
   #   KWIN_DRM_DEVICES = "/dev/dri/by-path/pci-0000:01:00.0-card:/dev/dri/by-path/pci-0000:00:02.0-card";
   # };
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="drm", KERNEL=="card*", DRIVERS=="nvidia", SYMLINK+="dri/by-driver/nvidia-card"
+    SUBSYSTEM=="drm", KERNEL=="card*", DRIVERS=="i915", SYMLINK+="dri/by-driver/intel-card"
+  '';
+
+  environment.sessionVariables = {
+    KWIN_DRM_DEVICES = "/dev/dri/by-driver/nvidia-card:/dev/dri/by-driver/intel-card";
+  };
 
   nix.settings = {
     substituters = [ "https://cache.nixos-cuda.org" ];
     trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
   };
-systemd.services.nvidia-uvm = {
-  description = "Load NVIDIA UVM module for CUDA";
-  wantedBy = [ "multi-user.target" ];
-  after = [ "systemd-modules-load.service" ];
-  serviceConfig = {
-    Type = "oneshot";
-    ExecStart = "/run/current-system/sw/bin/modprobe nvidia_uvm";
-    RemainAfterExit = true;
+
+  systemd.services.nvidia-uvm = {
+    description = "Load NVIDIA UVM module for CUDA";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-modules-load.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/modprobe nvidia_uvm";
+      RemainAfterExit = true;
+    };
   };
-};
 }
