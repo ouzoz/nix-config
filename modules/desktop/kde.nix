e3f582d95f24e78ee49d2632a67b07a1ebb83590
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-06 14:23:58 +0300

    Çrş 06 May 2026 14:23:58 +03

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -6,12 +6,4 @@
   services.displayManager.plasma-login-manager.enable = true;
 
   environment.systemPackages = with pkgs; [ wl-clipboard ];
-
-  environment.sessionVariables = {
-    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
 }
--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -22,9 +22,7 @@
 
   # environment.sessionVariables = {
     # GBM_BACKEND = "nvidia-drm";
-    # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     # LIBVA_DRIVER_NAME = "nvidia";
-    # NVIDIA_WAYLAND_DXGI = "1";
   # };
 
   nix.settings = {
