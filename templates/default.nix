fe28d018b2fb8798300413cdf76843e1bc8fcb7d
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-07 02:20:31 +0300

    Prş 07 May 2026 02:20:31 +03

--- templates/default.nix
+++ templates/default.nix
@@ -3,8 +3,7 @@
 {
   empty.path = ./empty;
   docs.path = ./docs;
-  tex.path = ./tex;
-  # ted.path = ./ted;
+  ted.path = ./ted;
   python.path = ./python;
   typescript.path = ./typescript;
   rust.path = ./rust;
