7544e4ea3a246e451cad922fb39d010f63467177
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-07 02:23:15 +0300

    Prş 07 May 2026 02:23:15 +03

--- templates/default.nix
+++ templates/default.nix
@@ -1,10 +1,34 @@
 { ... }:
 
 {
-  empty.path = ./empty;
-  docs.path = ./docs;
-  ted.path = ./ted;
-  python.path = ./python;
-  typescript.path = ./typescript;
-  rust.path = ./rust;
+  # empty.path = ./empty;
+  # docs.path = ./docs;
+  # ted.path = ./ted;
+  # python.path = ./python;
+  # typescript.path = ./typescript;
+  # rust.path = ./rust;
+empty = {
+    path = ./empty;
+    description = "A minimal empty flake template";
+  };
+  docs = {
+    path = ./docs;
+    description = "Documentation project template";
+  };
+  ted = {
+    path = ./ted;
+    description = "Ted project template";
+  };
+  python = {
+    path = ./python;
+    description = "Python development environment";
+  };
+  typescript = {
+    path = ./typescript;
+    description = "TypeScript/Node project template";
+  };
+  rust = {
+    path = ./rust;
+    description = "Rust Cargo project template";
+  };
 }
