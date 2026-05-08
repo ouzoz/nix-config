28bea762a26c63b34be7360fbae948262ca7baae
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-08 14:00:08 +0300

    Cum 08 May 2026 14:00:08 +03

--- modules/programs/nvim/lsp/ty.lua
+++ modules/programs/nvim/lsp/ty.lua
@@ -1,5 +1,5 @@
 return {
-  cmd = { 'ty', 'server' },
+  cmd = { 'uv', 'run', 'ty', 'server' },
   filetypes = { 'python' },
   root_markers = { 'ty.toml', 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git' },
 }
