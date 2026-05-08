e4efe37de155dbdc844ed8b683ebcf5a06e591eb
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-08 08:32:35 +0300

    Cum 08 May 2026 08:32:35 +03

--- modules/programs/development.nix
+++ modules/programs/development.nix
@@ -9,32 +9,9 @@
     texliveFull
     prettier
     cudatoolkit
-    gcc14
     bun
     uv
     rustup
     just
   ];
-
-  programs.nix-ld.libraries = with pkgs; [
-    cudatoolkit
-    glib
-    glibc
-    zlib
-    openssl
-    stdenv.cc.cc.lib
-  ];
-
-  environment.sessionVariables = {
-    CUDA_PATH = "${pkgs.cudatoolkit}";
-    CUDA_HOME = "${pkgs.cudatoolkit}";
-    CUDAToolkit_ROOT = "${pkgs.cudatoolkit}";
-    CUDAFLAGS = "-I${pkgs.cudatoolkit}/include";
-    NVCC_PREPEND_FLAGS = "-I${pkgs.cudatoolkit}/include";
-    CUDACXX = "${pkgs.cudatoolkit}/bin/nvcc";
-    CUDAHOSTCXX = "${pkgs.gcc14}/bin/g++";
-
-    CC = "${pkgs.gcc14}/bin/gcc";
-    CXX = "${pkgs.gcc14}/bin/g++";
-  };
 }
