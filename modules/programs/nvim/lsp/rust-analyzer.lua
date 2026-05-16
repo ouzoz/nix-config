e656632f5f86b7b7ce1aee209d641c28aaa14047
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-16 03:32:43 +0300

    Cts 16 May 2026 03:32:43 +03

--- modules/programs/nvim/lsp/rust-analyzer.lua
+++ modules/programs/nvim/lsp/rust-analyzer.lua
@@ -1,21 +1,5 @@
 return {
   cmd = { 'rust-analyzer' },
   filetypes = { 'rust' },
-  root_markers = {
-    'Cargo.toml',
-    '.git'
-  },
-  settings = {
-    ["rust-analyzer"] = {
-      imports = {
-        granularity = { group = "module" },
-        prefix = "self",
-      },
-      cargo = { buildScripts = { enable = true } },
-      checkOnSave = true,
-      check = { command = "clippy", },
-      procMacro = { enable = true },
-      diagnostic = { enable = true },
-    }
-  }
+  root_markers = { 'Cargo.toml' }
 }
