4a4a30fc3b26a4f19f4c184d14a6e7a8f8d9da9d
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-21 08:22:05 +0300

    Prş 21 May 2026 08:22:05 +03

--- modules/programs/foot.nix
+++ modules/programs/foot.nix
@@ -21,53 +21,54 @@
       };
       colors-dark = {
         # alpha = ;
-        cursor = "ffffff 000000";
-        selection-foreground = "000000";
+        cursor = "000000 ffffff";
+        selection-foreground = "ffffff";
         selection-background = "ece6f1";
-        foreground = "000000";
-        background = "ffffff";
-
-        regular0 = "000000";
-        regular1 = "ea1d3b";
-        regular2 = "009e00";
-        regular3 = "888600";
-        regular4 = "5675eb";
-        regular5 = "ab41eb";
-        regular6 = "009689";
-        regular7 = "cbc6d0";
-
-        bright0 = "65616a";
-        bright1 = "ea1d3b";
-        bright2 = "009e00";
-        bright3 = "888600";
-        bright4 = "5675eb";
-        bright5 = "ab41eb";
-        bright6 = "009689";
-        bright7 = "ffffff";
-
+        foreground = "ffffff";
+        background = "000000";
 
         # regular0 = "000000";
-        # regular1 = "ff3a4c";
-        # regular2 = "00b708";
-        # regular3 = "9c9b00";
-        # regular4 = "6687ff";
-        # regular5 = "bd56ff";
-        # regular6 = "00ad9f";
+        # regular1 = "ea1d3b";
+        # regular2 = "009e00";
+        # regular3 = "888600";
+        # regular4 = "5675eb";
+        # regular5 = "ab41eb";
+        # regular6 = "009689";
         # regular7 = "cbc6d0";
         #
         # bright0 = "65616a";
-        # bright1 = "ff3a4c";
-        # bright2 = "00b708";
-        # bright3 = "9c9b00";
-        # bright4 = "6687ff";
-        # bright5 = "bd56ff";
-        # bright6 = "00ad9f";
+        # bright1 = "ea1d3b";
+        # bright2 = "009e00";
+        # bright3 = "888600";
+        # bright4 = "5675eb";
+        # bright5 = "ab41eb";
+        # bright6 = "009689";
         # bright7 = "ffffff";
 
 
-        # "233" = "100d14";
-        # "235" = "232027";
-        # "239" = "4e4a53";
+        regular0 = "000000";
+        regular1 = "ff3a4c";
+        regular2 = "00b708";
+        regular3 = "9c9b00";
+        regular4 = "6687ff";
+        regular5 = "bd56ff";
+        regular6 = "00ad9f";
+        regular7 = "cbc6d0";
+
+        bright0 = "65616a";
+        bright1 = "ff3a4c";
+        bright2 = "00b708";
+        bright3 = "9c9b00";
+        bright4 = "6687ff";
+        bright5 = "bd56ff";
+        bright6 = "00ad9f";
+        bright7 = "ffffff";
+
+
+        "233" = "100d14";
+        "235" = "232027";
+        "239" = "4e4a53";
+
         "241" = "65616a";
         "246" = "96929b";
         "251" = "ece6f1";
