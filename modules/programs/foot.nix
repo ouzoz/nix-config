908ed1559f734fd24c5c13e15c2b8f8bb53e4c98
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-23 08:11:41 +0300

    Cts 23 May 2026 08:11:41 +03

--- modules/programs/foot.nix
+++ modules/programs/foot.nix
@@ -4,8 +4,8 @@
     enable = true;
     settings = {
       main = {
-        font = "Oziosevka:size=12";
-        line-height = 18.6;
+        font = "Oziosevka:size=14.4";
+        # line-height = 18.6;
         resize-delay-ms = 0;
         resize-keep-grid = "no";
         locked-title = "yes";
