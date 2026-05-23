6cab89e3477ae752cf7d01cf53b49c303c371440
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-24 00:12:14 +0300

    Sun May 24 00:12:14 +03 2026

--- modules/desktop/hyprland/hyprland.lua
+++ modules/desktop/hyprland/hyprland.lua
@@ -64,8 +64,8 @@ hl.config({
     blur = {
       enabled   = true,
       size      = 3,
-      passes    = 2,
-      vibrancy  = 0.24,
+      passes    = 1,
+      vibrancy  = 0.12,
     },
   },
   dwindle = {
