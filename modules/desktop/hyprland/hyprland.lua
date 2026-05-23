b4cc213c98659175f550adb380c6677041d4fd37
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-23 15:06:09 +0300

    Sat May 23 15:06:09 +03 2026

--- modules/desktop/hyprland/hyprland.lua
+++ modules/desktop/hyprland/hyprland.lua
@@ -1,9 +1,16 @@
 hl.monitor({
   output   = "eDP-1",
-  disabled = true,
-  mode     = "preferred",
-  position = "auto",
-  scale    = "auto",
+  disabled = false,
+  mode     = "1920x1080@165",
+  position = "1920x0",
+  scale    = 1,
+})
+
+hl.monitor({
+  output   = "HDMI-A-1",
+  mode     = "1920x1080@180",
+  position = "0x0",
+  scale    = 1,
 })
 
 local terminal    = "foot"
