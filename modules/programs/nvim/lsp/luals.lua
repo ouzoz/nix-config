ea40effb3117e89cea7309253af11bd6864c9177
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-31 00:55:00 +0300

    Paz 31 May 2026 00:55:00 +03

--- modules/programs/nvim/lsp/luals.lua
+++ modules/programs/nvim/lsp/luals.lua
@@ -14,15 +14,6 @@ return {
   settings = {
     Lua = {
       telemetry = { enable = false },
-      -- diagnostics = { globals = { "vim" } },
-      -- workspace = {
-      --   checkThirdParty = false,
-      --   library = vim.api.nvim_get_runtime_file("", true),
-      -- },
-      -- runtime = {
-      --   version = "LuaJIT",
-      --   path = vim.split(package.path, ";"),
-      -- },
     },
   },
 }
