9fd94ba028790877a50a08dbb4ad8a17fd2c097e
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-05-31 10:24:27 +0300

    Paz 31 May 2026 10:24:27 +03

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -4,10 +4,10 @@
   imports = [
     ../modules/desktop/fonts.nix
     ../modules/desktop/cursor.nix
+    ../modules/desktop/waybar.nix
+    ../modules/desktop/sound.nix
     ../modules/desktop/mako.nix
     ../modules/desktop/hypr
-    ../modules/desktop/waybar
-    ../modules/desktop/sound.nix
 
     ../modules/programs/gui.nix
   ];
