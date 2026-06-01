afcb02c6946559d5ec29a4603f31779263c90c03
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-01 14:10:21 +0300

    Pzt 01 Haz 2026 14:10:21 +03

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -40,19 +40,18 @@
   # };
 
   security.polkit.enable = true;
-  systemd.user.units."hyprpolkitagent.service".wantedBy = [ "graphical-session.target" ];
-  # systemd.user.services.hyprpolkitagent = {
-  #   description = "Hyprland Polkit Authentication Agent";
-  #   wantedBy = [ "graphical-session.target" ];
-  #   after = [ "graphical-session.target" ];
-  #   partOf = [ "graphical-session.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
-  #     Restart = "on-failure";
-  #     Slice = "session.slice";
-  #     TimeoutStopSec = "5sec";
-  #   };
-  # };
+  systemd.user.services.hyprpolkitagent = {
+    description = "Hyprland Polkit Authentication Agent";
+    wantedBy = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
+      Restart = "on-failure";
+      Slice = "session.slice";
+      TimeoutStopSec = "5sec";
+    };
+  };
 
   programs.dconf.profiles.user.databases = [
     {
