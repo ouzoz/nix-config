db2ea53c25d3f034643d5e448391cd099bc8d950
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-01 13:05:01 +0300

    Pzt 01 Haz 2026 13:05:01 +03

--- devshells.nix
+++ devshells.nix
@@ -7,7 +7,7 @@ let
       just
     ];
 
-    ShellHook = "";
+    shellHook = "";
   };
 
   shells = {
@@ -31,16 +31,6 @@ let
       ];
     };
   };
-
-  shells.default = {
-    packages = pkgs.lib.unique (
-      pkgs.lib.flatten (map (shell: shell.packages or [ ]) (builtins.attrValues shells))
-    );
-
-    shellHook = pkgs.lib.concatStringsSep "\n" (
-      map (shell: shell.shellHook or "") (builtins.attrValues shells)
-    );
-  };
 in
 builtins.mapAttrs (
   shellName: shell:
@@ -60,3 +50,14 @@ builtins.mapAttrs (
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
   }
 ) shells
+// {
+  default = {
+    packages = pkgs.lib.unique (
+      pkgs.lib.flatten (map (shell: shell.packages or [ ]) (builtins.attrValues shells))
+    );
+
+    shellHook = pkgs.lib.concatStringsSep "\n" (
+      map (shell: shell.shellHook or "") (builtins.attrValues shells)
+    );
+  };
+}
