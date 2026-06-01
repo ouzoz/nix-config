7aba51b13891762bcdd83ffa295e95c4518c24dd
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-01 13:26:49 +0300

    Pzt 01 Haz 2026 13:26:49 +03

--- devshells.nix
+++ devshells.nix
@@ -1,11 +1,13 @@
 { pkgs }:
 let
-  name = "system-config";
+  project = "system-config";
 
   base = {
     packages = with pkgs; [
       just
     ];
+
+    env = { };
 
     shellHook = "";
   };
@@ -32,32 +34,49 @@ let
     };
   };
 in
-builtins.mapAttrs (
-  shellName: shell:
-  let
-    packages = pkgs.lib.unique (base.Packages ++ (shell.packages or [ ]));
-    shellHook = pkgs.lib.concatStringsSep "\n" [
-      base.shellHook
-      (shell.shellHook or "")
-      ''
-        echo "- ${name} ${shellName}-shell activated."
-      ''
-    ];
-  in
-  pkgs.mkShell {
-    name = "${name}-${shellName}-shell";
-    inherit packages shellHook;
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
-  }
-) (shells
-// {
-  default = {
-    packages = pkgs.lib.unique (
-      pkgs.lib.flatten (map (shell: shell.packages or [ ]) (builtins.attrValues shells))
-    );
+builtins.mapAttrs
+  (
+    shellName: shell:
+    let
+      name = "${project}-${shellName}-shell";
+      packages = pkgs.lib.unique (base.packages ++ (shell.packages or [ ]));
+      shellHook = pkgs.lib.concatStringsSep "\n" [
+        base.shellHook
+        (shell.shellHook or "")
+        ''
+          echo "${name} activated."
+        ''
+      ];
+      env = (base.env or { }) // (shell.env or { });
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
+    in
+    pkgs.mkShell {
+      inherit
+        name
+        packages
+        env
+        shellHook
+        LD_LIBRARY_PATH
+        ;
+    }
+  )
+  (
+    shells
+    // {
+      default = {
+        packages = pkgs.lib.unique (
+          pkgs.lib.flatten (map (shell: shell.packages or [ ]) (builtins.attrValues shells))
+        );
 
-    shellHook = pkgs.lib.concatStringsSep "\n" (
-      map (shell: shell.shellHook or "") (builtins.attrValues shells)
-    );
-  };
-})
+        env =
+          envs:
+          pkgs.lib.foldl' (acc: env: acc // env) { } envs (
+            map (shell: shell.env or { }) (builtins.attrValues shells)
+          );
+
+        shellHook = pkgs.lib.concatStringsSep "\n" (
+          map (shell: shell.shellHook or "") (builtins.attrValues shells)
+        );
+      };
+    }
+  )
