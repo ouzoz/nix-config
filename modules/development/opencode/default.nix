ab53ba9517b8011c6c201f325b94ee4349a32e0c
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-01 09:47:41 +0300

    Pzt 01 Haz 2026 09:47:41 +03

--- modules/development/opencode/default.nix
+++ modules/development/opencode/default.nix
@@ -2,8 +2,6 @@
 {
   environment.sessionVariables = {
     OPENCODE_ENABLE_EXA = 1;
-    OPENCODE_CONFIG = "/etc/xdg/opencode/opencode.json";
-    OPENCODE_CONFIG_DIR = "/etc/xdg/opencode";
   };
 
   environment.shellAliases = {
@@ -14,22 +12,14 @@
     opencode
   ];
 
-  # system.userActivationScripts.linkOpencode = {
-  #   text = ''
-  #     mkdir -p ~/.config/opencode
-  #     ln -sfn /etc/nixos/modules/development/opencode/agents ~/.config/opencode/agents
-  #     ln -sfn /etc/nixos/modules/development/opencode/commands ~/.config/opencode/commands
-  #     ln -sfn /etc/nixos/modules/development/opencode/themes ~/.config/opencode/themes
-  #     ln -sfn /etc/nixos/modules/development/opencode/opencode.json ~/.config/opencode/opencode.json
-  #     ln -sfn /etc/nixos/modules/development/opencode/RULES.md ~/.config/opencode/RULES.md
-  #   '';
-  # };
-
-  environment.etc = {
-    "xdg/opencode/agents".source = ./agents;
-    "xdg/opencode/commands".source = ./commands;
-    "xdg/opencode/themes".source = ./themes;
-    "xdg/opencode/opencode.json".source = ./opencode.json;
-    "xdg/opencode/RULES.md".source = ./RULES.md;
+  system.userActivationScripts.linkOpencode = {
+    text = ''
+      mkdir -p ~/.config/opencode
+      ln -sfn /etc/nixos/modules/development/opencode/agents ~/.config/opencode/agents
+      ln -sfn /etc/nixos/modules/development/opencode/commands ~/.config/opencode/commands
+      ln -sfn /etc/nixos/modules/development/opencode/themes ~/.config/opencode/themes
+      ln -sfn /etc/nixos/modules/development/opencode/opencode.json ~/.config/opencode/opencode.json
+      ln -sfn /etc/nixos/modules/development/opencode/RULES.md ~/.config/opencode/RULES.md
+    '';
   };
 }
