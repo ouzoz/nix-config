355370b1f02e576d46093886641f39cfeb973c61
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-02 19:43:22 +0300

    Sal 02 Haz 2026 19:43:22 +03

--- modules/core/nvim/default.nix
+++ modules/core/nvim/default.nix
@@ -4,7 +4,6 @@ let
   treesitter-runtime = pkgs.callPackage ../../../packages/treesitter-runtime {
     parsers = with pkgs.tree-sitter-grammars; [
       tree-sitter-bash
-      tree-sitter-c-sharp
       tree-sitter-cmake
       tree-sitter-cpp
       tree-sitter-css
@@ -13,13 +12,12 @@ let
       tree-sitter-dockerfile
       tree-sitter-dot
       tree-sitter-embedded-template
-      tree-sitter-git-config
-      tree-sitter-git-rebase
-      tree-sitter-gitattributes
-      tree-sitter-gitcommit
-      tree-sitter-gitignore
+      # tree-sitter-git-config
+      # tree-sitter-git-rebase
+      # tree-sitter-gitattributes
+      # tree-sitter-gitcommit
+      # tree-sitter-gitignore
       tree-sitter-go
-      tree-sitter-haskell
       tree-sitter-html
       tree-sitter-ini
       tree-sitter-java
@@ -28,16 +26,12 @@ let
       tree-sitter-julia
       tree-sitter-just
       tree-sitter-latex
-      tree-sitter-llvm
       tree-sitter-log
       tree-sitter-make
       tree-sitter-nix
-      tree-sitter-ocaml
       tree-sitter-opencl
       tree-sitter-php
-      tree-sitter-powershell
       tree-sitter-python
-      tree-sitter-r
       tree-sitter-rust
       tree-sitter-sql
       tree-sitter-toml
