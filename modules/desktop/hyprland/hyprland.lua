7318451e9b8d503feb8eae232704c5f0183d9761
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-03 13:59:09 +0300

    Çrş 03 Haz 2026 13:59:09 +03

--- modules/desktop/hyprland/hyprland.lua
+++ modules/desktop/hyprland/hyprland.lua
@@ -68,7 +68,7 @@ local monitors = {
   end,
 }
 
-monitors[1]()
+monitors[3]()
 
 -- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
 -- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
