b48a12ae8f61f5c52f099e1d5f405a01c63d717b
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-05 23:45:57 +0300

    Cum 05 Haz 2026 23:45:57 +03

--- modules/desktop/fuzzel.nix
+++ modules/desktop/fuzzel.nix
@@ -9,7 +9,7 @@
       font = "Oziosevka:size=12";
       use-bold = true;
       placeholder = "";
-      prompt = "\" \"";
+      prompt = "\"\"";
       icons-enabled = true;
       match-counter = true;
       terminal = "foot  -a '{cmd}' -T '{cmd}' {cmd}";
