edfc79a6b03faa314ddd7dc602cf74c36a99681a
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-05 22:54:43 +0300

    Cum 05 Haz 2026 22:54:43 +03

--- modules/desktop/fuzzel.nix
+++ modules/desktop/fuzzel.nix
@@ -7,17 +7,23 @@
   environment.etc."xdg/fuzzel/fuzzel.ini".source = (pkgs.formats.ini {}).generate "fuzzel.ini" {
     main = {
       font = "Oziosevka:size=12";
-      line-height="18";
-      terminal = "foot";
+      use-bold = true;
+      placeholder = "";
       prompt = " ";
       icons-enabled = true;
+      match-counter = true;
+      terminal = "foot  -a '{cmd}' -T '{cmd}' {cmd}";
+      list-executables-in-path = false;
+      anchor = "top";
+
+      line-height="18";
       pad = "60x60";
       initial-color-theme = "dark";
       initial-window-size-chars = "12x50";
     };
 
     colors = {
-      background = "000000db";
+      background = "000000ea";
       text = "ffffff";
       selection = "232027";
     };
