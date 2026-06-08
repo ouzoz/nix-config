824f7a4d43f71e84f334c2e8826d978fd1189d83
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-08 11:02:25 +0300

    Pzt 08 Haz 2026 11:02:25 +03

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 { pkgs }:
 {
-  treesitter-runtime = pkgs.callPackage ./treesitter-runtime { pkgs = pkgs; };
+  treesitter-runtime = pkgs.callPackage ./treesitter-runtime { };
   oziosevka = pkgs.callPackage ./oziosevka { };
 }
