8ffdc7d1f1dc417708ae26a36a30bbc65ac8be36
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-06-08 10:55:34 +0300

    Pzt 08 Haz 2026 10:55:34 +03

--- hosts/ouz/configuration.nix
+++ hosts/ouz/configuration.nix
@@ -4,10 +4,10 @@
   system.stateVersion = "25.11";
   networking.hostName = "ouz";
 
-  config.my.mod.desktop.external = true;
-
-  specialisation.configuration.onthego = {
-    system.nixos.tags = [ "onthego" ];
-    config.my.desktop.external = lib.mkForce false;
-  };
+  # config.my.mod.desktop.external = true;
+  #
+  # specialisation.configuration.onthego = {
+  #   system.nixos.tags = [ "onthego" ];
+  #   config.my.desktop.external = lib.mkForce false;
+  # };
 }
