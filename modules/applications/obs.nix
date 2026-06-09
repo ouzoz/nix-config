3c5c498802fec6d55db0ecbd8cfe64a50c826767
ouzoz <ozkayaoguzhan67@gmail.com>
2026-06-09 18:00:07 +0300

    Sal 09 Haz 2026 18:00:07 +03

--- modules/applications/obs.nix
+++ modules/applications/obs.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ config, lib, ... }:
 {
-  programs.obs-studio = {
-    enable = true;
+  options.my.mod.applications = {
+    obs.enable = lib.mkEnableOption "obs studio";
+  };
+
+  config = lib.mkIf config.my.mod.applications.obs.enable {
+    programs.obs-studio.enable = true;
   };
 }
