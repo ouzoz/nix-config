29a80cd0d8a41cc02920dafdb0cc7e4f315373a3
ouzoz <ozkayaoguzhan67@gmail.com>
2026-06-11 12:42:20 +0300

    Prş 11 Haz 2026 12:42:20 +03

--- modules/core/nvim/lua/oz/colorscheme.lua
+++ modules/core/nvim/lua/oz/colorscheme.lua
@@ -311,4 +311,4 @@ ht('@markup.italic.markdown_inline', c.t, c.f2, italic)
 --customs
 hl('@property.css', '@keyword')
 hl('@property.json', '@keyword')
-hl('@property.yaml', '@keyword')
+hl('@property.yaml', '@function')
