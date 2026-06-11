5b5d954656a5b855727a078265df1fbdc806837f
ouzoz <ozkayaoguzhan67@gmail.com>
2026-06-11 13:09:18 +0300

    Prş 11 Haz 2026 13:09:18 +03

--- modules/core/nvim/lsp/tinymist.lua
+++ modules/core/nvim/lsp/tinymist.lua
@@ -4,6 +4,13 @@ return {
   root_markers = { '.git' },
   settings = {
     formatterMode = "typstyle",
+    completion = {
+      triggerOnSnippetPlaceholders = true,
+    },
+    lint = {
+      enabled = true,
+      when = "onType",
+    },
     semanticTokens = "enable"
   }
 }
