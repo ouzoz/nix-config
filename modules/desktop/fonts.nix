6563b716caf8f9e10fb1aea19eed95056a5c7190
ouzoz <ozkayaoguzhan67@gmail.com>
2026-06-17 22:07:49 +0300

    Çrş 17 Haz 2026 22:07:49 +03

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -5,12 +5,20 @@
     packages = with pkgs; [
       # corefonts
 
+      (google-fonts.override {
+        fonts = [
+          "Spectral"
+        ];
+      })
+
       my.pkgs.oziosevka
-      # source-code-pro
-      #
+      source-code-pro
+
       # source-sans
+      mona-sans
+
       # source-serif
-      #
+
       # crimson-pro
     ];
   };
