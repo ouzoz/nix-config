8127367b95a32cab94fbe618db7fd4449724371b
ouzoz <ozkayaoguzhan67@gmail.com>
2026-06-23 20:57:20 +0300

    add theme oklch

--- modules/applications/obs.nix
+++ modules/applications/obs.nix
@@ -1,10 +1,14 @@
 { config, lib, ... }:
+
+let
+  cfg = config.my.mod.applications;
+in
 {
   options.my.mod.applications = {
     obs.enable = lib.mkEnableOption "obs studio";
   };
 
-  config = lib.mkIf config.my.mod.applications.obs.enable {
+  config = lib.mkIf cfg.obs.enable {
     programs.obs-studio.enable = true;
   };
 }
