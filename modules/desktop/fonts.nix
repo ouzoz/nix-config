f078c04037d585782c705b34c61e385dbb42e581
ouzoz <ozkayaoguzhan67@gmail.com>
2026-07-02 17:12:02 +0300

    fontconfig properties

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,7 +1,21 @@
-{ pkgs, my, ... }: {
+{
+  pkgs,
+  my,
+  config,
+  ...
+}:
+
+let
+  cfg = config.my.theme.fonts;
+in
+{
   fonts = {
     enableDefaultPackages = false;
+    # enableGhostscriptFonts = false;
+    # fontDir.enable = false;
+
     packages = with pkgs; [
+      noto-fonts-color-emoji
       # corefonts
 
       (google-fonts.override { fonts = [ "Spectral" ]; })
@@ -18,5 +32,29 @@
       libre-baskerville
 
     ];
+
+    fontconfig = {
+      enable = true;
+      useEmbeddedBitmaps = true;
+      antialias = true;
+
+      hinting = {
+        enable = true;
+        autohint = false;
+        style = "slight";
+      };
+
+      subpixel = {
+        lcdfilter = "default";
+        rgba = "rgb";
+      };
+
+      defaultFonts = {
+        serif = [ cfg.serif ];
+        sansSerif = [ cfg.sans ];
+        monospace = [ cfg.mono ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
   };
 }
