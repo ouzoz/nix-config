90233dc97ce70991ccaaccf65073dd52579eea13
ouzoz <ozkayaoguzhan67@gmail.com>
2026-08-18 13:42:30 +0300

    move users to host configuration

--- hosts/ouz/configuration.nix
+++ hosts/ouz/configuration.nix
@@ -31,4 +31,19 @@ _:
       };
     };
   };
+
+  users.users.ouz = {
+    isNormalUser = true;
+    description = "ouz";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  my.home.users = {
+    ouz = {
+      emacs = true;
+    };
+  };
 }
